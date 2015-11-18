8adb6730f2e925c8bed35647cf0630288f8c31d7
Jackson Ruthe <18jacksonruthe@imac-c104-04.local>
2015-11-18 11:38:06 -0500

    Initial Commit

---
+++ Chapter7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chapter7
+//
+//  Created by Jackson Ruthe on 11/18/15.
+//  Copyright © 2015 Jackson Ruthe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
