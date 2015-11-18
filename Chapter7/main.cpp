c4d3f296698a28814ef7d83041cfb3f1e654d511
Jackson Ruthe <18jacksonruthe@imac-c104-04.local>
2015-11-18 12:02:02 -0500

    Initial commit

--- Chapter7/main.cpp
+++ Chapter7/main.cpp
@@ -5,11 +5,27 @@
 //  Created by Jackson Ruthe on 11/18/15.
 //  Copyright © 2015 Jackson Ruthe. All rights reserved.
 //
-
+#include <string>
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+int main()
+{
+    int inputOne = 0;
+    int inputTwo = 0;
+    
+    cout << "Enter an Even Int" << endl;
+    cin >> inputOne;
+    cout << "Enter an Int" << endl;
+    cin >> inputTwo;
+    if (inputOne % 2 == 0)
+    {
+        while (inputOne < inputTwo)
+        {
+            
+        }
+    }
+    else
+    {
+        printf("You entered an Odd Int");
+    }
 }
