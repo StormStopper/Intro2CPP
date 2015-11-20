e8e3e2bba4460eb3a4705de7e5369fd5623ffbaa
Jackson Ruthe <18jacksonruthe@imac-c104-04.local>
2015-11-20 12:07:23 -0500

    ARRAY FUN TIME!!!!!!

--- Chapter7/main.cpp
+++ Chapter7/main.cpp
@@ -1,27 +1,19 @@
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int inputOne = 0;
-    int inputTwo = 0;
-    int final;
-    cout << "Enter an Int" << endl;
-    cin >> inputOne;
-    cout << "Enter an Int" << endl;
-    cin >> inputTwo;
-    if (inputOne % 2  == 0)
-    {
-		for (; inputOne <= inputTwo; inputOne += 2)
-        {
-            final += inputOne;
-        }
-    }
-    else
-    {
-        inputOne = inputOne + 1;
-    }
-    cout << final << endl;
-    return 0;
+    int exspenseArraySize;
+    cout << "Enter SIze of Exspense Array";
+    cin >> exspenseArraySize;
+    int *exspense = new int[exspenseArraySize];
+    //int income = { };
+    //cout << "Enter Income";
+    //cin >> income;
+    cout << "Enter Expsense";
+    cin >> *exspense;
+    //cout << income;
+    cout << exspense;
 }
