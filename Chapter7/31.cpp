ddef7e2d4ecee138f4d2390f28bc06a1e81113eb
Jackson Ruthe <18jacksonruthe@imac-c104-04.local>
2015-11-20 11:57:24 -0500

    Stop

---
+++ Chapter7/31.cpp
@@ -0,0 +1,31 @@
+//
+//  31.cpp
+//  Chapter7
+//
+//  Created by Jackson Ruthe on 11/20/15.
+//  Copyright © 2015 Jackson Ruthe. All rights reserved.
+//
+
+#include "31.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    /*int i = 0;
+    int x = 0;
+    cout << "How many income number thingy entery";
+    cin >> i;
+    cout << "Entery the expsensies";
+    cin >> x;*/
+    int income = { };
+    int exspense ={ };
+    cout << "Enter Income";
+    cin >> income;
+    cout << "Enter Expsense";
+    cin >> exspense;
+    cout << income;
+    cout << exspense;
+    
+}
